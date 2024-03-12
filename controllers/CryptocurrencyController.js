4480ffcbefde3d2af5eeb0d56a241c7822e89b39
Bozhidar Kabzamalov <kabzamalov.bozhidar@gmail.com>
2024-03-12 04:49:11 +0200

    Add ESLint + Prettier

--- controllers/CryptocurrencyController.js
+++ controllers/CryptocurrencyController.js
@@ -1,20 +1,18 @@
 const axios = require('axios');
 
-module.exports.hehe = async (req, res, next) => {
+module.exports.getLatestCryptocurrency = async (req, res, next) => {
     try {
-        const response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=1,1027',
-            {
-                headers: {
-                    'X-CMC_PRO_API_KEY': 'fc84c397-229a-43cb-aab9-7c518b75ee0e',
-                    "Access-Control-Allow-Origin": "*",
-                }
-            }
-        )
-    
+        const response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=1,1027', {
+            headers: {
+                'X-CMC_PRO_API_KEY': 'fc84c397-229a-43cb-aab9-7c518b75ee0e',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+
         res.status(200).json({
-            data: response.data.data
-        })
+            data: response.data.data,
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 let cryptocurrencyController = require('../controllers/CryptocurrencyController');
 
-router.get('/cryptocurrency', cryptocurrencyController.hehe);
+router.get('/cryptocurrency', cryptocurrencyController.getLatestCryptocurrency);
 
 module.exports = router;
